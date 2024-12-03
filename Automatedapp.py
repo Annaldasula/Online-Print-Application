cfa5ef0bbddc5443038c7abcffc0fc96db11ecae
Annaldasula <102715951+Annaldasula@users.noreply.github.com>
2024-12-03 14:15:15 +0530

    Update Automatedapp.py

--- Automatedapp.py
+++ Automatedapp.py
@@ -777,7 +777,8 @@ News search: All Articles: entity mentioned at least once in the article"""
 
     text_box = slide.shapes.add_textbox(Inches(1.9), Inches(1.0), textbox_width, textbox_height)
     text_frame = text_box.text_frame
-    text_frame.text = f"{client_name} News Analysis By Media Research & Analytics Team"
+    text_frame.text = (f"{client_name}.\n"
+                      "News Analysis By Media Research & Analytics Team")
 
         
     # Set font size to 30 and make the text bold and white
