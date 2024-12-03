e71d9b3e22e262a1faac140997bfe55fd8ee98f0
Annaldasula <102715951+Annaldasula@users.noreply.github.com>
2024-12-03 14:44:33 +0530

    Update Automatedapp.py

--- Automatedapp.py
+++ Automatedapp.py
@@ -398,7 +398,7 @@ def add_table_to_slide(slide, df, title, textbox_text):
     textbox_right = Inches(0.25)
     textbox_top = Inches(0.8)  # Adjust top positioning as needed
     textbox_width = Inches(16)  # Adjust width
-    textbox_height = Inches(3)  # Adjust height
+    textbox_height = Inches(1)  # Adjust height
 
     text_box = slide.shapes.add_textbox(textbox_left, textbox_top, textbox_width, textbox_height)
     text_frame = text_box.text_frame
