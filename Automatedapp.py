0011c70fc018afb0657de66b9064c8e7d0eacf7d
Annaldasula <102715951+Annaldasula@users.noreply.github.com>
2025-01-09 10:42:00 +0530

    Update Automatedapp.py

--- Automatedapp.py
+++ Automatedapp.py
@@ -1755,7 +1755,7 @@ f"•The  journalists reporting on {client_name} and not on its competitors are 
                 img_path6 = generate_bar_pchart(dfs[4])  # Generate chart from first DataFrame
                 add_image_to_slide2(slide, img_path6)
 
-            if i == 8:
+            if i == 6:
                 wordcloud_path = generate_word_cloud(finaldata)  # Generate word cloud from DataFrame
                 add_image_to_slide11(slide, wordcloud_path)
               
